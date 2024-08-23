71ba8fbe652f8385c27a74f0fe9a5c6b1a06196d
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-08-23 14:25:22 -0600

    creacion de componentes de graficas con ejemplo comentado

---
+++ resources/js/MyComponents/Chart/RadialBar/CircleCustomAngle.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="chart">
+        <apexchart type="radialBar" height="390" :options="chartOptions" :series="series"></apexchart>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            // series: [76, 67, 61, 90],
+            // chartOptions: {
+            //     chart: {
+            //         height: 390,
+            //         type: 'radialBar',
+            //     },
+            //     plotOptions: {
+            //         radialBar: {
+            //             offsetY: 0,
+            //             startAngle: 0,
+            //             endAngle: 270,
+            //             hollow: {
+            //                 margin: 5,
+            //                 size: '30%',
+            //                 background: 'transparent',
+            //                 image: undefined,
+            //             },
+            //             dataLabels: {
+            //                 name: {
+            //                     show: false,
+            //                 },
+            //                 value: {
+            //                     show: false,
+            //                 }
+            //             },
+            //             barLabels: {
+            //                 enabled: true,
+            //                 useSeriesColors: true,
+            //                 offsetX: -8,
+            //                 fontSize: '16px',
+            //                 formatter: function (seriesName, opts) {
+            //                     return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex]
+            //                 },
+            //             },
+            //         }
+            //     },
+            //     colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
+            //     labels: ['Vimeo', 'Messenger', 'Facebook', 'LinkedIn'],
+            //     responsive: [{
+            //         breakpoint: 480,
+            //         options: {
+            //             legend: {
+            //                 show: false
+            //             }
+            //         }
+            //     }]
+            // },
+        }
+    },
+    props: {
+        series: Array,
+        chartOptions: Object,
+    }
+}
+</script>
---
+++ resources/js/MyComponents/Chart/Slope/BasicSlope.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="chart">
+        <apexchart type="line" height="350" width="400" :options="chartOptions" :series="series"></apexchart>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            // series: [
+            //     {
+            //         name: 'Blue',
+            //         data: [
+            //             {
+            //                 x: 'Jan',
+            //                 y: 43,
+            //             },
+            //             {
+            //                 x: 'Feb',
+            //                 y: 58,
+            //             },
+            //         ],
+            //     },
+            //     {
+            //         name: 'Green',
+            //         data: [
+            //             {
+            //                 x: 'Jan',
+            //                 y: 33,
+            //             },
+            //             {
+            //                 x: 'Feb',
+            //                 y: 38,
+            //             },
+            //         ],
+            //     },
+            //     {
+            //         name: 'Red',
+            //         data: [
+            //             {
+            //                 x: 'Jan',
+            //                 y: 55,
+            //             },
+            //             {
+            //                 x: 'Feb',
+            //                 y: 21,
+            //             },
+            //         ],
+            //     },
+            // ],
+            // chartOptions: {
+            //     chart: {
+            //         height: 350,
+            //         width: 400,
+            //         type: 'line',
+            //     },
+            //     plotOptions: {
+            //         line: {
+            //             isSlopeChart: true,
+            //         },
+            //     }
+            // },
+        }
+    },
+    props: {
+        chartOptions: Object,
+        series: Array,
+    }
+}
+</script>
