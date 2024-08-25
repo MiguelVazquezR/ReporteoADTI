3273212402cdfdf5937f517a4291c3b78bff25fd
Angel Vazquez <angelvazquez470@gmail.com>
2024-08-25 17:56:13 -0600

    graficas casi terminadas

--- resources/js/MyComponents/Chart/Column/WithRotatedLabels.vue
+++ resources/js/MyComponents/Chart/Column/WithRotatedLabels.vue
@@ -1,86 +1,98 @@
 <template>
     <div id="chart">
-        <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
+        <apexchart type="bar" height="280" :options="chartOptions" :series="series"></apexchart>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            // series: [{
-            //     name: 'Servings',
-            //     data: [44, 55, 41, 67, 22, 43, 21, 33, 45, 31, 87, 65, 35]
-            // }],
-            // chartOptions: {
-            //     annotations: {
-            //     points: [{
-            //         x: 'Bananas',
-            //         seriesIndex: 0,
-            //         label: {
-            //         borderColor: '#775DD0',
-            //         offsetY: 0,
-            //         style: {
-            //             color: '#fff',
-            //             background: '#775DD0',
-            //         },
-            //         text: 'Bananas are good',
-            //         }
-            //     }]
-            //     },
-            //     chart: {
-            //     height: 350,
-            //     type: 'bar',
-            //     },
-            //     plotOptions: {
-            //     bar: {
-            //         borderRadius: 10,
-            //         columnWidth: '50%',
-            //     }
-            //     },
-            //     dataLabels: {
-            //     enabled: false
-            //     },
-            //     stroke: {
-            //     width: 0
-            //     },
-            //     grid: {
-            //     row: {
-            //         colors: ['#fff', '#f2f2f2']
-            //     }
-            //     },
-            //     xaxis: {
-            //     labels: {
-            //         rotate: -45
-            //     },
-            //     categories: ['Apples', 'Oranges', 'Strawberries', 'Pineapples', 'Mangoes', 'Bananas',
-            //         'Blackberries', 'Pears', 'Watermelons', 'Cherries', 'Pomegranates', 'Tangerines', 'Papayas'
-            //     ],
-            //     tickPlacement: 'on'
-            //     },
-            //     yaxis: {
-            //     title: {
-            //         text: 'Servings',
-            //     },
-            //     },
-            //     fill: {
-            //     type: 'gradient',
-            //     gradient: {
-            //         shade: 'light',
-            //         type: "horizontal",
-            //         shadeIntensity: 0.25,
-            //         gradientToColors: undefined,
-            //         inverseColors: true,
-            //         opacityFrom: 0.85,
-            //         opacityTo: 0.85,
-            //         stops: [50, 0, 100]
-            //     },
-            //     }
-            // },
+            series: [{
+                name: 'Bolsas',
+                data: [0, 0, 1000, 6000, 3000, 2000, 3, 500]
+            }],
+            chartOptions: {
+                annotations: {
+                points: [{
+                    x: 'Desviación',
+                    seriesIndex: 0,
+                    label: {
+                    borderColor: '#000',
+                    offsetY: 0,
+                    style: {
+                        color: '#fff',
+                        background: '#775DD0',
+                    },
+                    text: 'Bolsas buenas',
+                    }
+                }]
+                },
+                chart: {
+                height: 280,
+                type: 'bar',
+                },
+                plotOptions: {
+                bar: {
+                    borderRadius: 10,
+                    columnWidth: '50%',
+                }
+                },
+                dataLabels: {
+                enabled: false
+                },
+                stroke: {
+                width: 0
+                },
+                grid: {
+                row: {
+                    colors: ['#fff', '#f2f2f2']
+                }
+                },
+                xaxis: {
+                    title: {
+                        text: 'Desviación',
+                    },
+                labels: {
+                    rotate: -45
+                },
+                categories: [-3, -2, -1, 0, 1, 2, 3, 4],
+                tickPlacement: 'on'
+                },
+                yaxis: {
+                    title: {
+                        text: 'Bolsas',
+                    },
+                },
+                fill: {
+                type: 'gradient',
+                gradient: {
+                    shade: 'light',
+                    type: "horizontal",
+                    shadeIntensity: 0.25,
+                    gradientToColors: undefined,
+                    inverseColors: true,
+                    opacityFrom: 0.85,
+                    opacityTo: 0.85,
+                    colorStops: [
+                        {
+                            offset: 0,
+                            color: '#17A281',
+                            opacity: 0.5
+                        },
+                        {
+                            offset: 100,
+                            color: '#19B38E',
+                            opacity: 0.9
+                        }
+                    ],
+                },
+                }
+            },
         }
     },
     props: {
-        series: Array,
-        chartOptions: Object,
+        // series: Array,
+        // chartOptions: Object,
     }
 }
 </script>
